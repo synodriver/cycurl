514904637f7a1c12f80bad51b4ac2034efb9897c
Yifei Kong <kong@yifei.me>
2023-03-28 19:41:56 +0800

    Change url to b2

--- prebuild.py
+++ prebuild.py
@@ -53,7 +53,7 @@ elif uname.system == "Windows":
     filename = "./curl-impersonate.tar.gz"
 else:
     if is_musl():
-        url = f"https://img.yifei.me/file/onefly-public/static/2023/libcurl-impersonate-v{VERSION}.{uname.machine}-linux-musl.zip"
+        url = f"https://f004.backblazeb2.com/file/onefly-public/static/2023/libcurl-impersonate-v{VERSION}.{uname.machine}-linux-musl.zip"
     else:
         url = f"https://github.com/lwthiker/curl-impersonate/releases/download/v{VERSION}/libcurl-impersonate-v{VERSION}.{uname.machine}-linux-gnu.tar.gz"
     filename = "./curl-impersonate.tar.gz"
