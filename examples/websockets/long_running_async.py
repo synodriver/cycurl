5d76d5a26414b0c56d898fae60d1703b94087e43
synodriver <diguohuangjiajinweijun@gmail.com>
2025-12-07 21:28:27 +0800

    fix localhost

--- examples/websockets/long_running_async.py
+++ examples/websockets/long_running_async.py
@@ -6,6 +6,7 @@ import cycurl
 async def main():
     async with cycurl.AsyncSession() as s:
         ws = await s.ws_connect("wss://api.gemini.com/v1/marketdata/BTCUSD")
+        # ws = await s.ws_connect("ws://127.0.0.1:10001/ws")
         print(
             "For websockets, you need to set $wss_proxy environment variable!\n"
             "$https_proxy will not work!"
